4ba9739f7230639a63e4f0a5aa7f1b6287f7eafd
claudiasmar <claudiasmar@educastur.org>
2025-09-01 16:50:06 +0200

    1º cambio proyecto React

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          hola mundo
+          hola mundo!
         </p>
         <a
           className="App-link"
